f1780dd1d27dcd39759590bb94b2e23b275ed776
Ronald Treur <ronald@funcracker.net>
2014-05-31 13:17:48 +0200

    Class-ified STSS. Now extends EventEmitter.

--- stss.js
+++ stss.js
@@ -1,10 +1,27 @@
 var fs = require('fs'),
 	chalk = require('chalk'),
 	async = require('async'),
+	EventEmitter = require('events').EventEmitter,
+	util = require('util'),
 	stss2scss = require('./lib/renderer/stss2scss'),
 	scss2css = require('./lib/renderer/scss2css'),
 	css2json = require('./lib/renderer/css2json'),
 	json2tss = require('./lib/renderer/json2tss');
+
+/**
+ * @class STSS
+ * @extends events.EventEmitter
+ *
+ * Manages STSS to TSS conversion.
+ * @author Ronald Treur
+ */
+var STSS = function() {
+	// Apply parent constructor
+	EventEmitter.call(this);
+};
+
+// Extend EventEmitter
+util.inherits(STSS, EventEmitter);
 
 
 /**
@@ -13,29 +30,33 @@ var fs = require('fs'),
  * This function executes asynchronously.
  *
  * @private
- * @param 	{String}	scss 		SCSS structured markup
- * @param 	{Object}	options		Dictionary containing instructions
+ * @param 	{String}	scss 				SCSS structured markup
+ * @param 	{Object}	options				Dictionary containing instructions
  * @param 	{Function} 	options.success 	Callback that will be called upon successful conversion
  * @param  	{Function}	[options.error]		Callback that will be called if conversion fails
  * @param 	{String} 	[options.shFile]	JSON file that contains additional shorthand notation to use during conversion
+ * @param 	{Function}	log 				Function that logs the current state of the process
  */
-function process(stss, options) {
+function process(stss, options, log) {
 	async.waterfall([
 		function(callback) {
 			var scss = stss2scss(stss, options);
+			log('scss', scss);
 			callback(null, scss);
 		},
 		function(scss, callback) {
-			console.log(scss);
 			var css = scss2css(scss, options);
+			log('css', css);
 			callback(null, css);
 		},
 		function(css, callback) {
 			var json = css2json(css, options);
+			log('json', JSON.stringify(json, null, 2));
 			callback(null, json);
 		},
 		function(json, callback) {
 			var tss = json2tss(json, options);
+			log('tss', tss);
 			callback(null, tss);
 		},
 	], function(err, tss) {
@@ -53,25 +74,64 @@ function process(stss, options) {
  * This function executes synchronously.
  *
  * @private
- * @param 	{String}	scss 		SCSS structured markup
- * @param 	{Object}	options		Dictionary containing instructions
+ * @param 	{String}	scss 				SCSS structured markup
+ * @param 	{Object}	options				Dictionary containing instructions
  * @param 	{Function} 	options.success 	Callback that will be called upon successful conversion
  * @param  	{Function}	[options.error]		Callback that will be called if conversion fails
  * @param 	{String} 	[options.shFile]	JSON file that contains additional shorthand notation to use during conversion
- * @return 	{String} 				TSS structured markup
+ * @param 	{Function}	log 				Function that logs the current state of the process
+ * @return 	{String} 						TSS structured markup
  */
-function processSync(stss, options) {
-	var scss = stss2scss(stss, options),
-		css = scss2css(scss, options),
-		json = css2json(css, options),
-		tss = json2tss(json, options);
-
+function processSync(stss, options, log) {
+	var scss = stss2scss(stss, options);
+	log('scss', scss);
+	var css = scss2css(scss, options);
+	log('css', css);
+	var json = css2json(css, options);
+	log('json', JSON.stringify(json, null, 2));
+	var tss = json2tss(json, options);
+	log('tss', tss);
 	return tss;
 }
 
-exports.render = function(options) {
-	var tss,
-		success = options.success;
+
+function parseOptions(options) {
+	if (options.shFile) {
+		if (!fs.existsSync(options.shFile)) {
+ 			throw new Error('Shorthand file does not exist: '+ options.shFile);
+	 	} else {
+	 		options.shorthand = require(options.shFile);
+	 	}
+	}
+
+	return options;
+}
+
+/**
+ * Render STSS to TSS using the supplied instructions.
+ *
+ * Either options.data or options.file need to be provided. If both are supplied, data takes precedence and file
+ * will be ignored.
+ *
+ * This function executes asynchronously.
+ * 
+ * @param 	{Object}	options				Dictionary containing instructions
+ * @param 	{String} 	[options.data]		STSS data, required if options.file is not passed
+ * @param 	{String} 	[options.file]		STSS file that is to be converted
+ * @param 	{Function} 	options.success 	Callback that will be called upon successful conversion
+ * @param  	{Function}	[options.error]		Callback that will be called if conversion fails
+ * @param 	{String} 	[options.shFile]	JSON file that contains additional shorthand notation to use during conversion
+ */
+STSS.prototype.render = function(options) {
+	var success = options.success,
+		logProcess = this.logProcess.bind(this),
+		tss;
+
+	try {
+		options = parseOptions(options);
+	} catch (err) {
+		return options.error && options.error(err);
+	}
 
 	options.success = function(tss) {
 		if (options.outFile) {
@@ -85,16 +145,13 @@ exports.render = function(options) {
  	};
 
  	if (options.data) {
-		tss = process(options.data, options, function(err, tss) {
-			if (err) { return options.error && options.error(err); }
-			options.success(tss);
-		});
+		process(options.data, options, logProcess);
 
 	} else if (options.file) {
 		fs.exists(options.file, function() {
 			fs.readFile(options.file, {encoding: 'utf8'}, function(err, data) {
 				if (err) { return options.error && options.error(err); }
-				process(data, options);
+				process(data, options, logProcess);
 			});
 		});
 
@@ -108,18 +165,26 @@ exports.render = function(options) {
  *
  * Either options.data or options.file need to be provided. If both are supplied, data takes precedence and file
  * will be ignored.
+ *
+ * This function executes synchronously.
  * 
  * @param 	{Object}	options				Dictionary containing instructions
  * @param 	{String} 	[options.data]		STSS data, required if options.file is not passed
  * @param 	{String} 	[options.file]		STSS file that is to be converted
  * @param 	{Function} 	options.success 	Callback that will be called upon successful conversion
  * @param  	{Function}	[options.error]		Callback that will be called if conversion fails
- * @param 	{String} 	[options.shFile]	JSON file that contains additional shorthand notation to use during conversion
+ * @param 	{String} 	[options.shFile]	JSON file that contains additional shorthands to use during conversion
  */
-exports.renderSync = function(options) {
-	var tss,
-		success = options.success,
-		data;
+STSS.prototype.renderSync = function(options) {
+	var success = options.success,
+		logProcess = this.logProcess.bind(this),
+		data, tss;
+
+	try {
+		options = parseOptions(options);
+	} catch (err) {
+		return options.error && options.error(err);
+	}
 
 	options.success = function(tss) {
 		if (options.outFile) {
@@ -134,9 +199,9 @@ exports.renderSync = function(options) {
 		}
  	};
 
-	if (options.data) {
+ 	if (options.data) {
 		try {
-			tss = processSync(options.data, options);
+			tss = processSync(options.data, options, logProcess);
 		} catch (err) {
 			return options.error && options.error(err);
 		}
@@ -150,7 +215,7 @@ exports.renderSync = function(options) {
 				return options.error && options.error(err);
 			}
 			try {
-				tss = processSync(data, options);
+				tss = processSync(data, options, logProcess);
 			} catch (err) {
 				return options.error && options.error(err);
 			}
@@ -163,3 +228,17 @@ exports.renderSync = function(options) {
 		return options.error && options.error('No input file or data supplied');
 	}
 };
+
+/**
+ * Log the conversion step that was just completed.
+ * 
+ * @param  {String} 	conversion 	Type of conversion just completed
+ * @param  {String} 	output 		Output of the phase that was just finished
+ * @fires conversionStep
+ */
+STSS.prototype.logProcess = function(conversion, output) {
+	this.emit('conversionStep', conversion, output);
+};
+
+// Return the Singleton
+module.exports = new STSS();
