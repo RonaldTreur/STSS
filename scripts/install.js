c5bc3ad0715d8190901330e647debc4113ef76ef
Ronald Treur <ronald@funcracker.net>
2014-06-11 22:44:16 +0200

    Cleaned up output and added some colors

--- scripts/install.js
+++ scripts/install.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    chalk = require('chalk'),
     path = require('path');
 
 function install(root) {
@@ -6,36 +7,36 @@ function install(root) {
         src = path.join(__dirname, '..', 'lib', 'alloy.jmk'),
         dst = path.join(appDir, 'alloy.jmk');
 
-    console.log('Trying to install alloy.jmk:');
-    console.log(' - working dir: '+ process.cwd());
-    console.log(' - project: '+ root);
-    console.log(' - app dir: '+ appDir);
-    console.log(' - JMK src: '+ src);
-    console.log(' - JMK dst: '+ dst);
+    console.log('Trying to install pre-compile hook in alloy.jmk...');
 
     // install alloy.jmk
     if (fs.existsSync(appDir)) {
         if (!fs.existsSync(dst)) {
             fs.writeFileSync(dst, fs.readFileSync(src));
+            console.log(chalk.green('Hook was successfully installed'));
         } else {
             var content = fs.readFileSync(dst);
             if (!/function\s+stss/.test(content)) {
                 fs.appendFileSync(dst, fs.readFileSync(src));
+                console.log(chalk.green('Hook was successfully installed'));
+            } else {
+                console.log(chalk.yellow('Hook was already installed'));
             }
         }
     } else {
-        console.log('Could not find the "app"-folder, compile-hook not installed!');
+        console.log(chalk.red('Could not find the "app"-folder in working directory ('+root+').\n') + chalk.red.bold('The pre-compile-hook was not installed!'));
     }
 }
-module.exports = install;
 
 if (require.main === module) {
     // Only run install if not installed globally
     if (process.env.global === true) {
-        console.log('STSS is installed globally, use "stss -jmk" in your project\'s root folder to install the compile-hook.');
+        console.log('STSS is installed globally\n' + chalk.blue('Use "stss -jmk" in your project\'s root folder to install the compile-hook.'));
         return;
     }
 
     var root = process.cwd().replace(/(.+[\/\\])node_modules.+/, '$1');
     install(root);
+} else {
+    module.exports = install;
 }
