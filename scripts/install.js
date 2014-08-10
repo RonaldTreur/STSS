6db07464762337c354b527a1925e25739ec30a77
Ronald Treur <ronald@funcracker.net>
2014-08-10 20:39:16 +0200

    Extended "install" script with update capabilities

--- scripts/install.js
+++ scripts/install.js
@@ -7,7 +7,7 @@ function install(root) {
         src = path.join(__dirname, '..', 'lib', 'alloy.jmk'),
         dst = path.join(appDir, 'alloy.jmk');
 
-    console.log('Trying to install pre-compile hook in alloy.jmk...');
+    console.log('Trying to add pre-compile hook to alloy.jmk...');
 
     // install alloy.jmk
     if (fs.existsSync(appDir)) {
@@ -15,12 +15,31 @@ function install(root) {
             fs.writeFileSync(dst, fs.readFileSync(src));
             console.log(chalk.green('Hook was successfully installed'));
         } else {
-            var content = fs.readFileSync(dst);
-            if (!/function\s+stss/.test(content)) {
+            var content = fs.readFileSync(dst, {encoding: 'utf8'});
+            if (!/function stss/.test(content)) {
                 fs.appendFileSync(dst, fs.readFileSync(src));
                 console.log(chalk.green('Hook was successfully installed'));
             } else {
-                console.log(chalk.yellow('Hook was already installed'));
+                try {
+                    var newVersion = process.env.npm_package_version.split('.'),
+                        newVersionNr = (newVersion[0]*1000 + newVersion[1])*1000 + newVersion[2],
+                        installedVersion = content.match(/function stss\(config, logger\) { \/\/--v(\d+)\.(\d+)\.(\d+)/).slice(1),
+                        installedVersionNr = (installedVersion[0]*1000 + installedVersion[1])*1000 + installedVersion[2];
+
+                    if (installedVersionNr >= newVersionNr) {
+                        console.log(chalk.yellow('Hook was already installed'));
+                    } else {
+                        var newSource = fs.readFileSync(src, {encoding: 'utf8'}),
+                            reFunction = /function stss\(config, logger\) { \/\/--v\d+\.\d+\.\d+([\s\S]+?)^}$/gm;
+
+                        newSource = newSource.match(reFunction)[0];
+                        content = content.replace(reFunction, newSource);
+                        fs.writeFileSync(dst, content);
+                        console.log(chalk.green('Hook was successfully updated to v'+newVersion.join('.')));
+                    }
+                } catch(e) {
+                    console.log(chalk.red('Hook could not be updated to v'+newVersion.join('.') + ' automatically, please update it manually'));
+                }
             }
         }
     } else {
